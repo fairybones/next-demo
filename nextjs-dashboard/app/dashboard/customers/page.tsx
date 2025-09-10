b550eafe16cbabd0fca76eb71f6c33f74bb331b7
Annie Schalnat <aeschalna@gmail.com>
2025-09-10 13:42:05 -0600

    supabase connected

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Customers</p>
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: customers } = await supabase.from("customers").select();
+  return (
+    <div>
+      <p>Customers</p>
+      <pre>{JSON.stringify(customers, null, 2)}</pre>
+    </div>
+  );
 }
--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Invoices</p>
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: invoices } = await supabase.from("invoices").select();
+  return (
+    <div>
+      <p>Invoices</p>
+      <pre>{JSON.stringify(invoices, null, 2)}</pre>
+    </div>
+  );
 }
