ee3cd80cf83578e94a630720c5c00cd01e5e8c88
Annie Schalnat <aeschalna@gmail.com>
2025-09-10 17:18:42 -0600

    Chapter 11 - Search and Pagination

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,12 +1,46 @@
 import { createClient } from "@/utils/supabase/server";
+import Pagination from "@/app/ui/invoices/pagination";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
   const supabase = await createClient();
   const { data: invoices } = await supabase.from("invoices").select();
+
   return (
     <div>
-      <p>Invoices</p>
-      <pre>{JSON.stringify(invoices, null, 2)}</pre>
+      {/* <p>Invoices</p>
+      <pre>{JSON.stringify(invoices, null, 2)}</pre> */}
+          <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
     </div>
   );
 }
